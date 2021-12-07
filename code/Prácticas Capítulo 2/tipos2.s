d2b09f2cd9dbcb2f0d38fe281805de2ac96ec558
Alonso <89488339+agro07@users.noreply.github.com>
2021-12-06 19:58:39 -0800

    Update tipos2.s

--- code/Prácticas Capítulo 2/tipos2.s
+++ code/Prácticas Capítulo 2/tipos2.s
@@ -17,19 +17,19 @@ b:	.word	4
 .global main
 
 main:
-	ldr r1, =a		@carga la direccion de memoria de a en r1	
+	ldr r1, =a	@carga la direccion de memoria de a en r1	
 	ldr r1, [r1]	@carga el valor de la direccion de memoria en r1
-	ldr r2, =b		@carga direccion de memoria de b en r2
+	ldr r2, =b	@carga direccion de memoria de b en r2
 	ldr r2, [r2]	@carga valor de direccion en r2
-	cmp r1, r2		@compara el valor de r1 y r2
-	bne else		@si no son iguales salta a etiqueta else, si son iguales sigue a etiqueta then
+	cmp r1, r2	@compara el valor de r1 y r2
+	bne else	@si no son iguales salta a etiqueta else, si son iguales sigue a etiqueta then
 
 then:
-	mov r0, #1		@asigna el valor 1 a r0
-	b final			@salta a etiqueta final
+	mov r0, #1	@asigna el valor 1 a r0
+	b final		@salta a etiqueta final
 	
 else:
-	mov r0, #0		@asigna el valor 0 a r0
+	mov r0, #0	@asigna el valor 0 a r0
 
 final:
-	bx lr			@instruccion de retorno
+	bx lr		@instruccion de retorno
