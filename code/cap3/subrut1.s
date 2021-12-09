a6f5715413aa1b7067bb9dee22db26c8a8ab3e25
Alonso <89488339+agro07@users.noreply.github.com>
2021-12-09 02:51:26 -0800

    Update subrut1.s

--- code/cap3/subrut1.s
+++ code/cap3/subrut1.s
@@ -18,13 +18,13 @@ const2 :	.word	12345
 .global myrand, mysrand
 
 myrand:
-	ldr r1, =seed	@ leo puntero a seed
-	ldr r0, [r1]	@ leo valor de seed
+	ldr r1, =seed		@ leo puntero a seed
+	ldr r0, [r1]		@ leo valor de seed
 	ldr r2, [r1, #4]	@ leo const1 en r2
-	mul r3, r0, r2	@r3 = seed*1103515245
+	mul r3, r0, r2		@r3 = seed*1103515245
 	ldr r0, [r1, #8]	@leo const2 en r0
-	add r0, r0, r3	@r0 = r3 + 12345
-	str r0, [r1]	@guardo en variable seed
+	add r0, r0, r3		@r0 = r3 + 12345
+	str r0, [r1]		@guardo en variable seed
 	
 /* Devuelven "seed >> 16 & 0x7fff".
 	Se evita el uso del AND */
