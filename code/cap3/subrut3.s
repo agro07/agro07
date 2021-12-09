5b6966c59bac3b5003998ebcc47a9df5e66fe198
Alonso <89488339+agro07@users.noreply.github.com>
2021-12-09 02:52:31 -0800

    Update subrut3.s

--- code/cap3/subrut3.s
+++ code/cap3/subrut3.s
@@ -26,8 +26,8 @@ main:
 /* bucle que imprime los 10 primeros valores */
 bucle:
 	mov r0, r4	@ tomo contador como parámetro
-	bl fibo	@ llamo a la función
-	mov r1, r0 @ paso resultado a r1
+	bl fibo		@ llamo a la función
+	mov r1, r0 	@ paso resultado a r1
 	ldr r0, =var1	@ pongo cadena en r0
 	bl printf	@ llamo a función printf
 	add r4, r4, #1	@ incremento contador de bucle
@@ -50,17 +50,17 @@ fibo:
 	movlo r0, #1	@ return 1
 	blo fib1
 	
-	sub r0, #1	@else
+	sub r0, #1		@else
 	str r0, [sp, #local1]	@salvo n-1 en [sp]
-	bl fibo	@ fibonacci(n-1)
+	bl fibo			@ fibonacci(n-1)
 	str r0, [sp, #local2]	@ salvo salida de fib. (n-1)
 	ldr r0, [sp, #local1]	@ recupero de la pila n-1
-	sub r0, #1	@ calculo n-2
-	bl fibo	@ fibonacci (n-2)
+	sub r0, #1		@ calculo n-2
+	bl fibo			@ fibonacci (n-2)
 	ldr r1, [sp, #local2]	@ recupero salida de fib (n-1)
-	add r0, r1	@ lo sumo a fib. (n-1)
+	add r0, r1		@ lo sumo a fib. (n-1)
 	
 fib1:
 	add sp, #length	@ libero espacio de var locales
 	pop {lr}	@ recupero registros, sólo lr
-	bx lr	@ salgo de la función
+	bx lr		@ salgo de la función
