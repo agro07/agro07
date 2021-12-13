89b338abb7c700084b3b0d216c49dd00978248b6
Alonso <89488339+agro07@users.noreply.github.com>
2021-12-12 23:22:36 -0800

    Update esbn2.s

--- code/cap4/esbn2.s
+++ code/cap4/esbn2.s
@@ -10,18 +10,18 @@
 */
 
 	.set GPBASE,	0x20200000
-	.set GPFSELO,	0x00
-	.set GPSETO, 	0x1c
+	.set GPFSEL0,	0x00
+	.set GPSET0, 	0x1c
 
 .text
 	ldr r0, =GPBASE
 
 /* guía bits	   xx999888777666555444333222111000 */
 	mov r1, #0b00001000000000000000000000000000
-	str r1, [r0, #GPFSELO] @ Configura GPIO 9
+	str r1, [r0, #GPFSEL0] @ Configura GPIO 9
 
 /* guía bits	   10987654321098765432109876543210 */
 	mov r1, #0b00000000000000000000001000000000
-	str r1, [r0, #GPSETO] @ Enciende GPIO 9
+	str r1, [r0, #GPSET0] @ Enciende GPIO 9
 
 infi: b infi
