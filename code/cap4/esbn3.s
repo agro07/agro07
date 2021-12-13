1d597a9473b9f59574304d2a2693f9be548e8787
Alonso <89488339+agro07@users.noreply.github.com>
2021-12-12 23:23:05 -0800

    Update esbn3.s

--- code/cap4/esbn3.s
+++ code/cap4/esbn3.s
@@ -10,21 +10,21 @@
 */
 
 	.set GPBASE,	0x20200000
-	.set GPFSELO,	0x00
-	.set GPSETO, 	0x1c
-	.set GPCLRO,	0x28
+	.set GPFSEL0,	0x00
+	.set GPSET0, 	0x1c
+	.set GPCLR0,	0x28
 
 .text
 	ldr r0, =GPBASE
 
 /* guía bits	   xx999888777666555444333222111000 */
 	mov r1, #0b00001000000000000000000000000000
-	str r1, [r0, #GPFSELO] @ Configura como salida
+	str r1, [r0, #GPFSEL0] @ Configura como salida
 
 /* guía bits	   10987654321098765432109876543210 */
 bucle:
 	mov r1, #0b00000000000000000000001000000000
-	str r1, [r0, #GPSETO] @ Enciende
+	str r1, [r0, #GPSET0] @ Enciende
 	mov r1, #0b00000000000000000000001000000000
-	str r1, [r0, #GPCLRO] @ Apaga
+	str r1, [r0, #GPCLR0] @ Apaga
 	b	bucle
